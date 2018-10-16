65d7741264209a5f48c43288f75f92c5f28b47f9
johnsontaiwo <josseytee@gmail.com>
2018-10-16 13:45:25 -0400

    create article && comment controllers

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,39 @@
+class ArticlesController < ApplicationController
+  def new
+    @article = Article.new
+    render json: @article
+  end
+
+  def index
+    @articles = Article.find_by(id: params[:id])
+    render json: @articles
+  end
+
+  def show
+    @article = Article.find_by(id: params[:id])
+    render json: @article
+  end
+
+  def update
+    @article = Article.find_by(id: params[:id])
+    if @article.update_attributes(article_params)
+      render json: @article
+    else
+      render :json=> @article.errors, :status=>422
+    end
+  end
+
+  def destroy
+    @article = Article.find_by(id: params[:id])
+    if @article.destroy
+      render :json=> { success: 'article was successfully deleted' }, :status=>201
+    else
+      render :json=> { error: 'article could not be deleted' }, :status=>422
+    end
+  end
+ 
+ private
+  def article_params
+    params.require(:article).permit(:title, :content, :author_name)
+  end
+end
