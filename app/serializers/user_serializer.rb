2a88e60b75bdf3c6e0d799507cbb4152e90645cb
johnsontaiwo <josseytee@gmail.com>
2018-10-19 13:31:02 -0400

    modified user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,11 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :email, :name, :articles
+  attributes :id, :email, :name, :article
+
+  def article
+    self.object.articles.map do |article|
+    {article_id: article.id,
+     article_title: article.title
+    }
+    end
+  end
 end
