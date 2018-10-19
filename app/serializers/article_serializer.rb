cbab8abb18c7a2c862e318db2be58500d7066059
johnsontaiwo <josseytee@gmail.com>
2018-10-19 13:43:29 -0400

    modified article serializer

--- app/serializers/article_serializer.rb
+++ app/serializers/article_serializer.rb
@@ -1,10 +1,19 @@
 class ArticleSerializer < ActiveModel::Serializer
-  attributes :id, :title, :content, :author
+  attributes :id, :title, :content, :author, :comment
 
   def author
     {author_id:
     self.object.user.id,
     author_name: self.object.user.name
     }
+  end
+
+  def comment
+    self.object.comments.map do |comment|
+      {
+       content: comment.content
+       #commenter: comment.user.name
+      }
+    end
   end
 end
