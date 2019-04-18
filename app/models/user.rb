2baaf4c116dcf54e7e6e4041d5b6109ba1eb3a62
johnsontaiwo <josseytee@gmail.com>
2019-04-18 12:35:10 -0400

    added validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_secure_password
   
   
-  validates :name, presence: true
+  validates :name, :age, :date_of_birth, presence: true
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true, length: { minimum: 6 }
 
